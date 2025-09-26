6e8fa1bf352e03308be94ef6d83df72aa52f031c
aldia_mechdream <futami16237@gmail.com>
2025-09-26 10:58:22 +0800

    fix: interface和type的区别找到了，web.d.ts中，interface和type内容完全一样，但是interface和Record<string, AnyFn>不兼容，但type是兼容的

--- src/web/web.d.ts
+++ src/web/web.d.ts
@@ -21,19 +21,6 @@ interface TabArgs {
 
 type CreateMainPageArgs = WorkspaceModalArgs & ComponentControlsArgs;
 
-// fixme 这个和Record<string, AnyFn>不兼容，但是下面的type是兼容的
-interface W {
-  'render-tab': (workspace: Workspace) => HTMLDivElement[];
-  'new-workspace': () => void;
-  'add-current-tab': () => void;
-  'edit-workspace': (workspace: Workspace) => void;
-  'delete-workspace': (workspaceId: string) => void;
-  'open-workspace': (workspaceId: string) => void;
-  'toggle-workspace': (workspaceId: string) => void;
-  'remove-tab': (workspaceId: string, tabId: number) => void;
-  'toggle-tab-pin': (workspaceId: string, tabId: number) => void;
-}
-
 type WorkspaceEventMap = {
   'render-tab': (workspace: Workspace) => HTMLDivElement[];
   'new-workspace': () => void;
