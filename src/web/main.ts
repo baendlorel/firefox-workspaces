b3c8e78bb4e1555ab1cfc880aa82521278729d3c
aldia_mechdream <futami16237@gmail.com>
2025-09-26 17:01:59 +0800

    feat: form-modal改名为editor

--- src/web/main.ts
+++ src/web/main.ts
@@ -6,13 +6,13 @@ import header from './components/workspace/header.js';
 import controls from './components/workspace/controls.js';
 import list from './components/workspace/list.js';
 import emptyState from './components/workspace/empty-state.js';
-import formModal from './components/workspace/form-modal.js';
+import editor from './components/workspace/editor.js';
 import tabs from './components/workspace/tabs.js';
 
 export function createMainPage() {
   const bus = new EventBus<WorkspaceEditorEventMap>();
-  const fm = formModal(bus);
-  $id('app').append(header(), controls(bus), list(bus), emptyState(bus), fm.el);
+  const editorEl = editor(bus);
+  $id('app').append(header(), controls(bus), list(bus), emptyState(bus), editorEl.el);
   // only for registering the 'render-tab' event
   tabs(bus);
 
@@ -31,6 +31,6 @@ export function createMainPage() {
      */
     on,
 
-    getEditingWorkspace: fm.getEditingWorkspace,
+    getEditingWorkspace: editorEl.getEditingWorkspace,
   };
 }
