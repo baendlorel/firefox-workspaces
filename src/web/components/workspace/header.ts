b602c1210cf1cd25b7e9bf23eba8e9ecabc4b1bf
aldia_mechdream <futami16237@gmail.com>
2025-09-27 19:46:36 +0800

    feat: 书签图标完成； 但是不再使用，改为btn-trans，更合适一些

--- src/web/components/workspace/header.ts
+++ src/web/components/workspace/header.ts
@@ -1,20 +1,11 @@
 import { EventBus } from 'minimal-event-bus';
 import { btn, div, h } from '@/lib/dom.js';
 
-import createNewWorkspaceSvg from '@web/assets/plus.svg?raw';
-import addTabSvg from '@web/assets/add-tab.svg?raw';
-
 export default (bus: EventBus<WorkspaceEditorEventMap>) => {
-  const newWorkspace = btn('btn btn-text');
-  newWorkspace.style.width = '36px';
-  newWorkspace.style.height = '36px';
-  newWorkspace.innerHTML = createNewWorkspaceSvg;
+  const newWorkspace = btn('btn btn-trans', 'New');
 
-  // todo 或者做成一个红色书签挂在header上？
-  const addCurrentTab = btn('btn btn-text', 'Add Current');
-  addCurrentTab.style.width = '36px';
-  addCurrentTab.style.height = '36px';
-  addCurrentTab.innerHTML = addTabSvg;
+  // const addCurrentTab = btn('bookmark', 'Add Tab');
+  const addCurrentTab = btn('btn btn-trans', 'Add Tab');
 
   newWorkspace.addEventListener('click', () => bus.emit('edit', null));
   addCurrentTab.addEventListener('click', () => bus.emit('add-current-tab'));
