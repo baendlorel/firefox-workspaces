f33d30ea292a26f5606a6d6298334188a57e9ee5
aldia_mechdream <futami16237@gmail.com>
2025-09-30 20:38:14 +0800

    chore: logger改成class，这样高亮是绿色的更好看一点

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,20 +1,20 @@
-export const logger = {
-  info(func: string, ...message: any[]) {
-    console.log(`%c[__NAME__] ${func}: `, 'color:blue', ...message);
-  },
-  warn(func: string, ...message: any[]) {
-    console.log(`%c[__NAME__] ${func}:`, 'color:orange', ...message);
-  },
-  error(func: string, ...message: any[]) {
-    console.log(`%c[__NAME__] ${func}:`, 'color:red', ...message);
-  },
-  debug(func: string, message: string) {
-    console.log(`%c[__NAME__] ${func}:`, 'color:purple', ...message);
-  },
-  WorkspaceNotFound(func: string, id: string) {
-    console.warn(`[__NAME__] ${func}: Workspace with id ${id} not found`);
-  },
-  TabNotFoundInWorkspace(func: string, id: string, tabId: number) {
-    console.warn(`[__NAME__] ${func}: Tab ${tabId} not found in workspace ${id}`);
-  },
-};
+export class logger extends null {
+  static info(func: string, ...message: any[]) {
+    console.log(`%c[__NAME__ info] ${func}: `, 'color:blue', ...message);
+  }
+  static warn(func: string, ...message: any[]) {
+    console.log(`%c[__NAME__ warn] ${func}:`, 'color:orange', ...message);
+  }
+  static error(func: string, ...message: any[]) {
+    console.log(`%c[__NAME__ error] ${func}:`, 'color:red', ...message);
+  }
+  static debug(func: string, message: string) {
+    console.log(`%c[__NAME__ debug] ${func}:`, 'color:purple', ...message);
+  }
+  static WorkspaceNotFound(func: string, id: string) {
+    logger.error(func, `Workspace with id ${id} not found`);
+  }
+  static TabNotFoundInWorkspace(func: string, id: string, tabId: number) {
+    logger.error(func, `Tab ${tabId} not found in workspace ${id}`);
+  }
+}
